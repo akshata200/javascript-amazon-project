8d16f3872806a53ae3510e07a621929823a44792
Akshata Ubale <akshata2202.01@gmail.com>
2026-01-02 19:46:20 +0530

    Changed Product list to come from Data/Product

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-const products = [
-    {
-        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating : {
-            star: 4.5,
-            count : 87
-        },
-        priceCents : 1090
-    },
-    {
-        image : 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketballlack and Gray Athletic Cotton Socks - 6 Pairs',
-        rating : {
-            star: 4.0,
-            count : 127
-        },
-        priceCents : 2095
-    },
-    {
-        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'A dults Plain Cotton T-Shirt - 2 Pack',
-        rating : {
-            star: 4.5,
-            count : 56
-        },
-        priceCents : 799
-    }
-];
+
+/* product list comes from folder data/product.js. Since we load product.js first in our amazon.html, 
+we can use product list form Product.js in amazom.js */
 
 let productHTMLElement = '';
 products.forEach((product)=>{
@@ -43,7 +17,7 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
