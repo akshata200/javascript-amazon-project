e1a0c9301b35f572bdac865cb98d62aa554d5a05
Akshata Ubale <akshata2202.01@gmail.com>
2026-01-11 12:24:33 +0530

    create automated Jasmine test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,24 @@
 
 //localStorage.removeItem('cart')
+export let cart;
 
+loadFromStorage();
 
-export let cart = (localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) :
-    [
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId : '1'
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId : '3'
-        }
-    ];
-//console.log(cart)
+export function loadFromStorage() {
+    cart = (localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) :
+        [
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: '3'
+            }
+        ];
+}
 
 
 function saveToLocalStorage() {
@@ -25,8 +28,9 @@ function saveToLocalStorage() {
 export function addToCart(productId) {
     let matchingItem;
     let quantityClass = `.js-quantity-selector-${productId}`;
-    const selectedQuantity = document.body.querySelector(quantityClass).value;
-    console.log(selectedQuantity);
+    const selectedQuantityElem = document.body.querySelector(quantityClass);
+    let selectedQuantity = selectedQuantityElem ? selectedQuantityElem.value : 1;
+    //console.log(selectedQuantity);
 
     cart.forEach((cartItem) => {
         if (cartItem.productId === productId)
@@ -40,7 +44,7 @@ export function addToCart(productId) {
         cart.push({
             productId: productId,
             quantity: Number(selectedQuantity),
-            deliveryOptionId : '1'
+            deliveryOptionId: '1'
         });
     }
     saveToLocalStorage();
@@ -74,7 +78,7 @@ export function updatecartItemQuantity(productId, newQuantity) {
     }
 }
 
-export function updateDeliveryOption(productId, newDeliveryOptionId){
+export function updateDeliveryOption(productId, newDeliveryOptionId) {
     const [cartItem] = cart.filter(cartItem => cartItem.productId === productId);
     cartItem.deliveryOptionId = newDeliveryOptionId;
     saveToLocalStorage();
---
+++ jasmineTests/data/cartTest.js
@@ -0,0 +1,44 @@
+import { cart, addToCart, loadFromStorage } from "../../data/cart.js";
+
+describe("test suite : addToCart",()=>{
+    it("add an existing product to cart",()=>{
+        // it should update the quantity of that existing product
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([{
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1,
+                deliveryOptionId: '1' 
+            }])
+        });
+
+        loadFromStorage();
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+
+        expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart.length).toEqual(1);
+        expect(cart[0].quantity).toEqual(2);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
+
+    it("add a new product to cart",()=>{
+        // mocking test case
+
+        spyOn(localStorage,'setItem');
+        // read it like : mock localStorage.getItem and return resposnse as whatever we return from callFake
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([]);
+        });
+        // console.log(localStorage.getItem('cart'));
+        loadFromStorage();
+
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart.length).toEqual(1);
+        // we can use toHaveBeenCalledTimes only if we are mocking that method
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart[0].quantity).toEqual(1);
+
+    });
+});
