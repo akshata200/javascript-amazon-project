fc7b4e1c1d79bf6fba1ac1309e9c635942ee25f5
Akshata Ubale <akshata2202.01@gmail.com>
2026-01-12 23:22:53 +0530

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,35 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords
+
+  }
+
+
+  getStarImageUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `${formatCurrency(this.priceCents)}`;
+  }
+
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -657,7 +689,12 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+});
+
+
+
 
 export function getProductById(productId){
   const [product] = products.filter(product => product.id === productId);
